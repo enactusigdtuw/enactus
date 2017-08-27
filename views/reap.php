7ab516f99852b6865524568b44f07d84028c5d7d
enactusigdtuw <31151741+enactusigdtuw@users.noreply.github.com>
2017-08-27 21:20:49 +0530

    Update reap.php

--- views/reap.php
+++ views/reap.php
@@ -7,45 +7,20 @@
                 <div class="border" data-position="left"></div>
                 <div class="border" data-position="bottom"></div>
                 <div class="intro-paragraph">
-                    <p>reap intro......... video behind......... if we want some qoutes behind ........aim of reap etc........Feed is an intelligent property rights and payments platform, using intelligent software and digital security that goes well beyond 'military-grade' to give users true ownership of their data and IP.</p>
-                    <p>Feed facilitates trusted exchanges of users' progressively-perfecting data assets with businesses, researchers, and governments in a <b>trusted</b>, audited, and independently verifiable manner; on their own terms and conditions.</p>
-                </div>
+				<h1 style="color:#ffff00;">#Reap</h1>
+                    <p>Reap is a social initiative taken up by Enactus IGDTUW with an aim to encourage the reuse and recycle of plastic.</p>
+                </div>                    
                 <div class="videobg-container"></div>
                 <div class="starwars-container">
                     <div class="starwars-wrapper">
-                        <p>When you want something, </p>
-                        <p>all the universe conspires </p>
-                        <p>in helping you to achieve it.</p>
-                        <p><strong><i><small>Paulo Coelho</small></i></strong></p>
+                        <p>REDUCE </p>
+                        <p>REUSE </p>
+                        <p>RECYCLE  </p>
                         <p><br><br></p>
-                        <p><br><br></p>
-                        <p>Feed is that conspiracy:</p>
-                        <p>the conspiracy of trust.</p>
-                        <p><br><br></p>
-                        <p><br><br></p>
-                        <p>Trust is the single</p>
-                        <p>most important ingredient </p>
-                        <p>missing from digital relationships.</p>
-                        <p><br><br></p>
-                        <p><br><br></p>
-                        <p>Boston Consulting Group </p>
-                        <p>and the World Economic Forum </p>
-                        <p>forecast the digital economy </p>
-                        <p>to be worth between </p>
-                        <p><strong>1.5 and 2.5 trillion dollars</strong></p>
-                        <p><strong>by 2016.</strong></p>
-                        <p><br><br></p>
-                        <p><br><br></p>
-                        <p>The difference </p>
-                        <p>between those numbers </p>
-                        <p>is trust.</p>
-                        <p><br><br></p>
-                        <p><br><br></p>
-                        <p style="white-space: normal">Feed is a digital mechanism of <strong>trust</strong></p>
+						<p style="white-space: normal"><strong>REAP</strong></p>
                     </div>
                 </div>
-
-               <a href="#" class="scroll-down" style="position:absolute;bottom:15%;left:50%;z-index:50;"><img src="images/logo/down.png" style="height:40px;width:40px;"></a>
+				<a href="#" class="scroll-down"><img/></a>
             </div>
 			
 			<div id="story" class="cineslider-slide">
@@ -54,11 +29,15 @@
                 <div class="border" data-position="left"></div>
                 <div class="border" data-position="bottom"></div>
                 <div class="intro-paragraph">
-                    <p>business model of reap............Feed is an intelligent property rights and payments platform, using intelligent software and digital security that goes well beyond 'military-grade' to give users true ownership of their data and IP.</p>
-                    <p>Feed facilitates trusted exchanges of users' progressively-perfecting data assets with businesses, researchers, and governments in a <b>trusted</b>, audited, and independently verifiable manner; on their own terms and conditions.</p>
-                </div>
+				<h1 style="color:#ffff00;">#Business Model</h1>
+                    <p>Our target audience comprises of the informal-pickers. Through field visits and research, we found out that the informal-pickers get paid less as compared to the actual rates of the same plastic bottles sold off to the factories.<br>
+We are targeting the environment issues of plastic waste.We researched and worked upon possible development of tools and machines to form products which can be pitched back in the market as commercialized products. </p>
+<p style="margin:10px;color:#ffcc00;">Our model involves creating a shredder which converts plastic bottles into smaller bits and pieces. The beneficiaries themselves collect the plastic bottles, shred and transport them. Thus, the removal of the middle men helps generate extra income for the ragpickers.</p><p>
+Moreover, the maintenance of financial records and transaction details by the workers provide them with a technical mind and an entrepreneurial skill set.
+</p>
+                </div>  
                 <div class="background" style="background-image:url(images/bg/39-dark.jpg);"></div>
-				<a href="#" class="scroll-down" style="position:absolute;bottom:15%;left:50%;z-index:50;"><img src="images/logo/down.png" style="height:40px;width:40px;"></a>		
+				<a href="#" class="scroll-down"><img/></a>
             </div>
 		 
 		    <div id="whatis" class="cineslider-slide">
@@ -67,69 +46,12 @@
                 <div class="border" data-position="left"></div>
                 <div class="border" data-position="bottom"></div>
                 <div class="intro-paragraph">
-                    <p>goal and objective of reap............Feed is an intelligent property rights and payments platform, using intelligent software and digital security that goes well beyond 'military-grade' to give users true ownership of their data and IP.</p>
-                    <p>Feed facilitates trusted exchanges of users' progressively-perfecting data assets with businesses, researchers, and governments in a <b>trusted</b>, audited, and independently verifiable manner; on their own terms and conditions.</p>
-                </div>
+				<h1 style="color:#ffff00;">#Objective</h1>
+                    <p>The project aims at recycling plastic bottles and plastic products with the help of machines to form commercial products that otherwise end up in the landfills and thus, clog the environment for decades.</p>
+                </div>  
                 <div class="background" style="background-image:url(images/bg/44-dark.jpeg);"></div>
-				<a href="#" class="scroll-down" style="position:absolute;bottom:15%;left:50%;z-index:50;"><img src="images/logo/down.png" style="height:40px;width:40px;"></a>		
             </div>
 			
-			<div id="igdtuw" class="cineslider-slide">
-                <div class="border" data-position="top"></div>
-                <div class="border" data-position="right"></div>
-                <div class="border" data-position="left"></div>
-                <div class="border" data-position="bottom"></div>
-                <div class="intro-paragraph">
-                    <p>our vision of reap...Feed is an intelligent property rights and payments platform, using intelligent software and digital security that goes well beyond 'military-grade' to give users true ownership of their data and IP.</p>
-                    <p>Feed facilitates trusted exchanges of users' progressively-perfecting data assets with businesses, researchers, and governments in a <b>trusted</b>, audited, and independently verifiable manner; on their own terms and conditions.</p>
-                </div>
-                <div class="background" style="background-image:url(images/bg/47-dark.jpg);"></div>
-				<a href="#" class="scroll-down" style="position:absolute;bottom:15%;left:50%;z-index:50;"><img src="images/logo/down.png" style="height:40px;width:40px;"></a>		
-            </div>
-
-			<div id="projects" class="cineslider-slide">
-                <div class="border" data-position="top"></div>
-                <div class="border" data-position="right"></div>
-                <div class="border" data-position="left"></div>
-                <div class="border" data-position="bottom"></div>
-
-                <div class="splitter3">
-                    <div class="splitter-item" id="a">
-                        <div class="vertical-container">
-                            <div class="vertical-content">
-                                <a href="#/feed-app" class="splitter-button">event 1</a>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="splitter-item" id="b">
-                        <div class="vertical-container">
-                            <div class="vertical-content">
-                                <a href="#/contact" class="splitter-button">event 2</a>
-                            </div>
-                        </div>
-                    </div>
-					<div class="splitter-item" id="c">
-                        <div class="vertical-container">
-                            <div class="vertical-content">
-                                <a href="#/contact" class="splitter-button">event 3</a>
-                            </div>
-                        </div>
-                    </div>					
-                </div>
-				<a href="#" class="scroll-down" style="position:absolute;bottom:15%;left:50%;z-index:50;"><img src="images/logo/down.png" style="height:40px;width:40px;"></a>		
-            </div>
-			
-            <div id="impact" class="cineslider-slide">
-                <div class="border" data-position="top"></div>
-                <div class="border" data-position="right"></div>
-                <div class="border" data-position="left"></div>
-                <div class="border" data-position="bottom"></div>
-                <div class="intro-paragraph">
-                    <p>impact of reap...Feed is an intelligent property rights and payments platform, using intelligent software and digital security that goes well beyond 'military-grade' to give users true ownership of their data and IP.</p>
-                    <p>Feed facilitates trusted exchanges of users' progressively-perfecting data assets with businesses, researchers, and governments in a <b>trusted</b>, audited, and independently verifiable manner; on their own terms and conditions.</p>
-                </div>
-                <div class="background" style="background-image:url(images/bg/21-light.jpg);"></div>	
-            </div> 
         </div>
     </div>
 </div>
