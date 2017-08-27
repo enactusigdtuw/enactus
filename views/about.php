6bbb0599e1d237bc79f9d943f275e48b200a1178
enactusigdtuw <31151741+enactusigdtuw@users.noreply.github.com>
2017-08-27 21:18:47 +0530

    Update about.php

--- views/about.php
+++ views/about.php
@@ -7,36 +7,37 @@
                 <div class="border" data-position="left"></div>
                 <div class="border" data-position="bottom"></div>
                 <div class="intro-paragraph">
-                    <p>about intro will write about enactus.....what is is blah blah............Feed is an intelligent property rights and payments platform, using intelligent software and digital security that goes well beyond 'military-grade' to give users true ownership of their data and IP.</p>
-                    <p>Feed facilitates trusted exchanges of users' progressively-perfecting data assets with businesses, researchers, and governments in a <b>trusted</b>, audited, and independently verifiable manner; on their own terms and conditions.</p>
+				<h1 style="color:white"><font color="red">EN</font>trepreneurial.<font color="red">ACT</font>ion.<font color="red">US</font></h1>
+                    <p style="color:#ff3300;">Enactus is an organisation committed to Entrepreneurial Action for sustainable development.<br>
+					It is aimed at resolving the problems in society by establishing a business model run by the marginalized section of society.</p>
                 </div>
                 <div class="videobg-container"></div>
-				<a href="#" class="scroll-down" style="position:absolute;bottom:15%;left:50%;z-index:50;"><img src="images/logo/down.png" style="height:40px;width:40px;"></a>
-            </div>
-		 
-		    <div id="whatis" class="cineslider-slide">
-                <div class="border" data-position="top"></div>
-                <div class="border" data-position="right"></div>
-                <div class="border" data-position="left"></div>
-                <div class="border" data-position="bottom"></div>
-                <div class="intro-paragraph">
-                    <p>will write the full form of enactus(in this use dark colored text)............Feed is an intelligent property rights and payments platform, using intelligent software and digital security that goes well beyond 'military-grade' to give users true ownership of their data and IP.</p>
-                    <p>Feed facilitates trusted exchanges of users' progressively-perfecting data assets with businesses, researchers, and governments in a <b>trusted</b>, audited, and independently verifiable manner; on their own terms and conditions.</p>
-                </div>
-                <div class="background" style="background-image:url(images/bg/4-light.jpg);"></div>
-				<a href="#" class="scroll-down" style="position:absolute;bottom:15%;left:50%;z-index:50;"><img src="images/logo/down.png" style="height:40px;width:40px;"></a>
-            </div>
-			
+				 <a href="#" class="scroll-down"><img/></a>
+            </div>		 
+		    
 			<div id="igdtuw" class="cineslider-slide">
                 <div class="border" data-position="top"></div>
                 <div class="border" data-position="right"></div>
                 <div class="border" data-position="left"></div>
                 <div class="border" data-position="bottom"></div>
                 <div class="intro-paragraph">
-                    <p>will write about our college igdtuw its history.....etc...Feed is an intelligent property rights and payments platform, using intelligent software and digital security that goes well beyond 'military-grade' to give users true ownership of their data and IP.</p>
-                    <p>Feed facilitates trusted exchanges of users' progressively-perfecting data assets with businesses, researchers, and governments in a <b>trusted</b>, audited, and independently verifiable manner; on their own terms and conditions.</p>
+				<h1 style="color:#ffff00;">#Who we are</h1>
+                    <p style="color:#fff;">Situated in the old city of Delhi, Indira Gandhi Delhi Technical University for Women is the first all women technical college of India.<br>IGDTUW focuses on imparting quality technical education to students while encouraging a variety of interests such as entrepreneurship, literature and management.</p>
                 </div>
                 <div class="background" style="background-image:url(images/bg/igdtuw.jpg);"></div>
+				 <a href="#" class="scroll-down"><img/></a>
+            </div>
+			<div id="whatis" class="cineslider-slide">
+                <div class="border" data-position="top"></div>
+                <div class="border" data-position="right"></div>
+                <div class="border" data-position="left"></div>
+                <div class="border" data-position="bottom"></div>
+                <div class="intro-paragraph">
+				<h1 style="color:#ff0000;">#Our Story</h1>
+                    <p style="color:#663300;">Bringing together the passionate and driven people of our campus, we started our chapter of ENACTUS over two years ago with the goal to look for opportunities to improve the livelihood of people in need by making them self sufficient and self reliant.<br>On 16th October 2015, we launched our first project: Khidmat. Initially Khidmat was a tiffin service where we helped underprivileged women to set up their own businesses by utilising their cooking skills. We have branched out to catering service and food stalls to target a larger demographic and provide opportunities to more women in need.<br>To tackle the problem of increasing use of plastic, we started our second project in July 2016: REAP. By devising a tool which shreds plastic bottles of different shapes into thin long strips, Project REAP aims to reduce the production of plastic and recycle the existing bottles to minimize the damage done to the environment. These strips of plastic are modified and woven together to design a range of commercial products. To expand this project, we are planning to create a set of machineries which would shred, mould and transform plastic bottles into sheets which can be used to make viable products.<br>In July 2016, Enactus IGDTUW participated in the Enactus Nationals, held in Mumbai and emerged victorious in the Rookie League, becoming the first all girls team ever to win the competition. It was a proud moment for all the members who represented the team in Nationals and for all the other members as well, when the team stood on stage and lifted the trophy which substantiated all the hard work.<br>Since then, we have worked to expand these projects, targeting new customers and creating new machineries. We are dedicated towards connecting more and more people with our organisation so as to create sustainable employment opportunities for them and to help them become entrepreneurs.
+</p>
+                </div>
+                <div class="background" style="background-image:url(images/bg/empty1.jpg);"></div>
             </div>
 			
          </div>
