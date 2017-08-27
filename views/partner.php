d93d7b232b4cb9c645b88598e629eb7afaa8ae34
enactusigdtuw <31151741+enactusigdtuw@users.noreply.github.com>
2017-08-27 21:21:18 +0530

    Update partner.php

--- views/partner.php
+++ views/partner.php
@@ -7,6 +7,7 @@
                 <div class="border" data-position="left"></div>
                 <div class="border" data-position="bottom"></div>
 				<div class="intro-paragraph">
+				<h1 style="color:#ffff00;">#Our Partners</h1>
                     <p>partners or sponsors of enactus igdtuw............Feed is an intelligent property rights and payments platform, using intelligent software and digital security that goes well beyond 'military-grade' to give users true ownership of their data and IP.</p>
                     <p>Feed facilitates trusted exchanges of users' progressively-perfecting data assets with businesses, researchers, and governments in a <b>trusted</b>, audited, and independently verifiable manner; on their own terms and conditions.</p>
                 </div>
