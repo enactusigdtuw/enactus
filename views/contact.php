7527b3e348cbb54804d2912d91e96bcc073a3330
enactusigdtuw <31151741+enactusigdtuw@users.noreply.github.com>
2017-08-28 09:16:55 +0530

    Update contact.php

--- views/contact.php
+++ views/contact.php
@@ -1,14 +1,3 @@
-<?php
-$connection = mysql_connect("localhost", "root", ""); 
-$db = mysql_select_db("enactus", $connection);
-if (isset($_POST['submit'])) {
-    $name = $_POST['name'];
-	$email = $_POST['email'];
-	$msg = $_POST['msg'];
-$query = mysql_query("insert into enactus(name, email, msg) values ('$name', '$email', '$msg')");
-}
-mysql_close($connection); 
-?>
 <div id="schedule-page" class="page">
     <div class="border" data-position="top"></div>
     <div class="border" data-position="right"></div>
@@ -16,23 +5,22 @@ mysql_close($connection);
     <div class="border" data-position="bottom"></div>
     <div class="vertical-container">
         <div class="vertical-content">
-            <div class="container">
-			
-			<div id="heading">Contact Us</div>
-			
+            <div class="container">			
+			<div id="heading">Contact Us</div>			
 			<div id="text">If you are interested in collaborating with us or have any queries, feedback or suggestions regarding our projects, feel free to contact us. We'd love to hear from you!</div>
-
 			<div id="alt_contact">
 				about the members that can be contacted or alternative contact information
                 </div>                    			                
-				<form class="nl-form" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post" target="_top" accept-charset="UTF-8">		
-				<legend style="text-align:center;font-size:25px;color:#ff6600;text-transform:uppercase;">Get in touch!</legend>
-                    <input type="text" name="name" placeholder="Name" required><br>
-                    <input type="email" name="email" placeholder="Email Address" required><br>
-                    <input type="textarea" name="msg" placeholder="Message" required><br>
-					<button class="nl-submit" type="submit" name="submit">Submit</button>
-                </form>				
-            </div>
+				</div>
+				<div class="map-container">
+        <h1 class="map-title">WHERE WE ARE</h1>
+        <a href="https://www.google.co.in/maps/place/Indira+Gandhi+Delhi+Technical+University+for+Women/@28.665236,77.2301802,17z/data=!3m1!4b1!4m5!3m4!1s0x390cfd0683919c3b:0xf5fc331b74c2b9e2!8m2!3d28.665236!4d77.2323689">Indira Gandhi Delhi Technical University for Women</a>
+        <p>Kashmere Gate, Opp. St. James Church<br>New Delhi, Delhi 110006</p>
+        <div class="embed-responsive">
+          <div class="embed-responsive-item map">
+          </div>
+        </div>
+    </div>
         </div>
     </div>
 </div>
